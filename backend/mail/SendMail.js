f7074017f98ac0191484db3ecdf6f3bd144198c6
Raushankumar4 <raushankumarguptag@gmail.com>
2026-03-05 17:29:01 +0530

    payemet error

--- backend/mail/SendMail.js
+++ backend/mail/SendMail.js
@@ -4,9 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  // host: "smtp.gmail.com",
+  // port: 465,
+  // secure: true,
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
